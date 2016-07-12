5182ac8dd9bf34226d762cc76fea47adcb6d05a2
Hua Zhou <hwachou@gmail.com>
2016-07-11 19:53:45 -0700

    locate jupyter executable

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,15 @@
-using Documenter, SnpArrays
+using Documenter, SnpArrays, IJulia
+import Conda
 
-run(`jupyter nbconvert --to markdown ./docs/snparray.ipynb --output ./docs/src/man/snparray.md`)
+if success(`which jupyter`)
+  jupyter_program = "jupyter"
+else
+  jupyter_program = joinpath(Conda.SCRIPTDIR, "jupyter")
+end
+run(`$jupyter_program nbconvert --to markdown ./docs/snparray.ipynb --output ./docs/src/man/snparray.md`)
 makedocs()
 deploydocs(
-  deps   = Deps.pip("mkdocs", "python-markdown-math", "jupyter"),
+  deps   = Deps.pip("mkdocs", "python-markdown-math"),
   repo   = "github.com:OpenMendel/SnpArrays.jl.git",
   julia  = "0.4",
   osname = "osx"
