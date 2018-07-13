ea086fc629214e7bced9fea66bb7a1aaadd0ef07
Hua Zhou <huazhou@ucla.edu>
2018-07-13 09:01:59 -0700

    docs cannot be built on osx; switch to linux

--- docs/make.jl
+++ docs/make.jl
@@ -6,5 +6,5 @@ deploydocs(
   deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
   repo   = "github.com:OpenMendel/SnpArrays.jl.git",
   julia  = "0.6",
-  osname = "osx"
+  osname = "linux"
   )
