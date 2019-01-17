4109f15346e4d5594ccfbb1fb1f958b4f26294db
Seyoon <syko0507@snu.ac.kr>
2019-01-16 23:26:08 -0500

    Revert "added snpdata, splitting and merging"
    This reverts commit a9465f380f1c0731a1521af78f50260dff7d6a16.

--- src/SnpArrays.jl
+++ src/SnpArrays.jl
@@ -4,14 +4,10 @@ module SnpArrays
 
 using LinearAlgebra, Missings, Mmap, SparseArrays, Statistics, StatsBase
 import Base: IndexStyle, convert, copyto!, eltype, getindex, setindex!, length, size
-import DataFrames: DataFrame, rename!
-import DelimitedFiles: readdlm, writedlm
-import Glob: glob
 import LinearAlgebra: mul!
 import Statistics: mean, std, var
 import StatsBase: counts
-export AbstractSnpArray, SnpArray, SnpBitMatrix, SnpData
-export split_plink, merge_plink, write_plink
+export AbstractSnpArray, SnpArray, SnpBitMatrix
 export counts, grm, maf, mean, minorallele, missingpos, missingrate, std, var
 export ADDITIVE_MODEL, DOMINANT_MODEL, RECESSIVE_MODEL
 
@@ -20,9 +16,8 @@ const DOMINANT_MODEL = Val(2)
 const RECESSIVE_MODEL = Val(3)
 
 include("snparray.jl")
-include("filter.jl")
-include("snpdata.jl")
 include("grm.jl")
+include("filter.jl")
 include("linalg.jl")
 
 datadir(parts...) = joinpath(@__DIR__, "..", "data", parts...)
