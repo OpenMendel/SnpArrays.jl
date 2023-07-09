fa5cfbaef51840648fd691e5baa020e5744fe67f
Kaitlyn Marlorgit config --global user.name Kaitlyn <kaitlynjmarlor@gmail.com>
2023-07-09 18:40:57 +0900

    Add unit tests for iterator

--- src/iterator.jl
+++ src/iterator.jl
@@ -2,7 +2,7 @@ mutable struct SnpArrayIterator <: VariantIterator
    snpdata::SnpData
 end
 
-struct SnpArrayIndex <: Variant
+mutable struct SnpArrayIndex <: Variant
     index::Int
 end 
 
@@ -10,12 +10,15 @@ end
     SnpArray
 end
 
-function Base.iterate(itr::SnpArrayIterator, state=itr.index)
+function Base.iterate(itr::SnpArrayIterator, state=1)
+    if state <= 0
+        throw(BoundsError(itr, state))
+    end
     if state > size(itr.snpdata.snp_info,2)
         return nothing
     else
-        state = state + 1
         result = (view(itr.snpdata.snp_info, :, state), state+1)
+        state = state + 1
         return result
     end
 end
@@ -28,70 +31,45 @@ end
     SnpArrayIterator(snpdata)
 end
 
-function iterator(snpdata::SnpData; startidx=1)::SnpArrayIterator
-    if startidx > size(snpdata,2)
-        return nothing
-    else
-        SnpArrayIndex(startidx)
-        return SnpArrayIterator(snpdata)
-    end
-end
 
 function iterator(s::SnpData; startidx=1)
     iterator = SnpArrayIterator(s)
 end
 
 function chrom(s::SnpData, startidx=1)::String
-    result = view(s.snp_info,startidx,1)
+    result = s.snp_info[startidx,1]
     return result
 end
 
 function pos(s::SnpData, startidx=1)::Int
-    result = view(s.snp_info,startidx,2)
+    result = s.snp_info[startidx,2]
     return result
 end
 
 function rsid(s::SnpData, startidx=1)::String
-    result = view(s.snp_info,startidx,3)
+    result = s.snp_info[startidx,3]
     return result
 end
 
 function alleles(s::SnpData, startidx=1)::Vector{String}
-    allele1 = view(s.snp_info,startidx,5)
-    allele2 = view(s.snp_info,startidx,6)
+    allele1 = s.snp_info[startidx,5]
+    allele2 = s.snp_info[startidx,6]
     return [allele1, allele2]
 end
 
 function alt_allele(s::SnpData, startidx=1)::String
-    alt = view(s.snp_info,startidx,6)
+    alt = s.snp_info[startidx,6]
     return alt
 end
 
 function ref_allele(s::SnpData, startidx=1)::String
-    ref = view(s.snp_info,v,5)
+    ref = s.snp_info[startidx,5]
     return ref
 end
 
-function mafCount(s::SnpData,startidx=1)::Int
-    n00 = 0
-    n01 = 0
-    n11 = 0
-    for i in startidx:size(s,1)
-        row = s[i,:]
-        totalCount += 1
-        if row[5] == 0 && row[6] == 0
-            n00 += 1
-        else if row[5] == 0 && row[6] == 1
-            n01 += 1
-        else if row[5] == 1 && row[6] == 1
-            n11 += 1
-        end
-    end
-    
-    return n00,n01,n11,totalCount
-end
 
-function maf(s::SnpData, startidx=1)
+
+function maf(s::SnpData, startidx=1)::Float
     results = mafCount(s)
     if results[1] <= results[2] && results[1] <= results[3]
         return float(results[1]) / float(results[4])
