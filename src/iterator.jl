52d8ce214dbdd631257d53f9aeee4845108cab86
Kaitlyn Marlorgit config --global user.name Kaitlyn <kaitlynjmarlor@gmail.com>
2023-10-10 15:55:32 -0700

    hwepval

--- src/iterator.jl
+++ src/iterator.jl
@@ -29,38 +29,62 @@ end
     return size(itr.snpdata.snp_info, 2)
 end
 
-function iterator(s::SnpData; startidx=1)
+function iterator(s::SnpData; snpindex::SnpArrayIndex)
     iterator = SnpArrayIterator(s)
 end
 
-function chrom(s::SnpData, startidx=1)::String
-    result = s.snp_info[startidx,1]
+function chrom(s::SnpData, snpindex::SnpArrayIndex)::String
+    result = s.snp_info[snpindex.index,:chromosome]
     return result
 end
 
-function pos(s::SnpData, startidx=1)::Int
-    result = s.snp_info[startidx,2]
+function pos(s::SnpData, snpindex::SnpArrayIndex)::Int
+    result = s.snp_info[snpindex.index,:position]
     return result
 end
 
-function rsid(s::SnpData, startidx=1)::String
-    result = s.snp_info[startidx,3]
+
+function rsid(s::SnpData, snpindex::SnpArrayIndex)::String
+    result = s.snp_info[snpindex.index,:snpid]
     return result
 end
 
-function alleles(s::SnpData, startidx=1)::Vector{String}
-    allele1 = s.snp_info[startidx,5]
-    allele2 = s.snp_info[startidx,6]
+#SnpData subtype of Genetic Data
+
+function alleles(s::SnpData, snpindex::SnpArrayIndex)::Vector{String}
+    allele1 = s.snp_info[snpindex.index,:allele1]
+    allele2 = s.snp_info[snpindex.index,:allele2]
     return [allele1, allele2]
 end
 
-function alt_allele(s::SnpData, startidx=1)::String
-    alt = s.snp_info[startidx,6]
+function alt_allele(s::SnpData, snpindex::SnpArrayIndex)::String
+    alt = s.snp_info[snpindex.index,:allele2]
     return alt
 end
 
-function ref_allele(s::SnpData, startidx=1)::String
-    ref = s.snp_info[startidx,5]
+function ref_allele(s::SnpData, snpindex::SnpArrayIndex)::String
+    ref = s.snp_info[snpindex.index,:allele1]
     return ref
 end
 
+function maf(s::SnpData, snpindex::SnpArrayIndex)
+    array = s.snparray
+    return maf(array)
+end
+
+function hwepval(s::SnpData, snpindex::SnpArrayIndex)'
+# Extract the genotype data for a specific SNP using snpindex
+    genotypes = s.snparray[snpindex, :]
+
+    # Calculate the observed genotype frequencies
+    observed_hom_ref = sum(genotypes .== 0) / length(genotypes)
+    observed_het = sum(genotypes .== 1) / length(genotypes)
+    observed_hom_alt = sum(genotypes .== 2) / length(genotypes)
+
+    # Calculate the expected genotype frequencies under HWE
+    total_alleles = 2 * s.people
+    expected_hom_ref = (observed_hom_ref + observed_het / 2)
+    expected_het = observed_het
+    expected_hom_alt = (observed_hom_alt + observed_het / 2)
+    
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -817,6 +817,7 @@ test_snparray_iterator(f)
 f_iterator = SnpArrayIterator(f)
 @test length(f_iterator) == 6
 @test iterate(f_iterator,5) == (["1", "1", "1", "1", "1", "1", "1"],6)
+# (SnpArrayIndex(5), 6)
 @test iterate(f_iterator,6) == (["2", "2", "2", "2", "2", "2", "2"],7)
 @test_throws BoundsError iterate(f_iterator,0)
 
@@ -826,4 +827,8 @@ test_snparray_iterator(g)
 g_iterator = SnpArrayIterator(g)
 @test iterate(g_iterator,1) == (["1"],2)
 @test_throws BoundsError iterate(g_iterator,0)
+
+h = create_dummy("test4", 4, 4)
+@show h
+
 
